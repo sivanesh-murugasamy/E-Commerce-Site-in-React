3e7f0b40d5df6248d3dff70af67478e86e84c504
ranjithsrk30 <ranjithkumars.21msc@kongu.edu>
2024-06-07 20:53:06 +0530

    first commit

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import './DescriptionBox.css';
+
+
+const DescriptionBox=()=>{
+ return(
+<div className='descriptionbox'>
+<div className="descriptionbox-navigator">
+    <div className="description-nav-box">Description</div>
+    <div className="description-nav-box fade">Reviews (122)</div>
+</div>
+ <div className="descriptionbox-description">
+    <p>
+"Men's Fashion and Clothing E-commerce Platform" is a web-based application designed to revolutionize the way customers shop for men's apparel online. 
+This platform acts as a seamless bridge between the company and its customers, offering a wide range of fashion items with just a few clicks. 
+Administered by an admin, the website stays up-to-date with the latest trends and inventory by constantly adding new clothing items and ac-cessories. 
+</p>
+
+<p>
+Customers can explore the website at their leisure, browsing through various cate-gories and collections to find the perfect pieces for their wardrobe.
+ From casual wear to formal attire, the platform provides detailed information about each item, including sizes, colors, and availability. This ensures that customers have all the necessary details to make informed purchasing decisions.
+</p>
+ </div>
+</div>
+ )
+}
+
+export default DescriptionBox
